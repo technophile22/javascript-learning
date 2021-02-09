d3d70075f8b6b51e9b66f3005ba1d515c774f0d9
technophile22 <raghavmangal22@gmail.com>
2021-02-09 17:32:37 +0530

    variables-form

---
+++ 02basics/02variables-form.js
@@ -0,0 +1,26 @@
+const uid = "abc123";
+
+var fullName = "Raghav Mangal";
+var email = "raghavmangal22@gmail.com";
+var password = "123456";
+var confirmPassword = "123546";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+//fullName = prompt("Enter your name");
+// to take input from user
+
+console.log("Full Name is:" + fullName);
+console.log(email);
+console.log(uid);
+
+console.log(`
+    With Unique ID: ${uid}
+    User is : ${fullName}
+    and his email is : ${email}
+    and uses the password : ${password}
+`);
+
+
+
+
