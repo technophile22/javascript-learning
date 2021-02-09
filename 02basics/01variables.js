3df885ed74686455ca0d52cad6c419443181dd76
technophile22 <raghavmangal22@gmail.com>
2021-02-09 17:15:42 +0530

    first commit, variables and datatypes

---
+++ 02basics/01variables.js
@@ -0,0 +1,16 @@
+var fullName = "Raghav Mangal";
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = true;
+//smaller case means reserved keywords
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+paymentMode = "Credit Card";
+console.log(paymentMode);
+
