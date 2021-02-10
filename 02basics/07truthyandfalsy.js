eb4dd4f024495c92f04df20ed6dae65fb5011b45
technophile22 <raghavmangal22@gmail.com>
2021-02-10 16:19:26 +0530

    coercion and falsy

---
+++ 02basics/07truthyandfalsy.js
@@ -0,0 +1,23 @@
+
+//undefined
+//""
+//null
+//Nan
+//these are falsy values
+
+//coercion
+//for loose check
+var user = "2";
+ if(2 == user){
+     console.log("condition is true");
+}
+
+//for strict check
+var user = "2";
+ if(2 === user){
+     console.log("condition is true");
+}
+
+ console.log(2+2);
+ console.log("2" + 2);
+ console.log(2 + "2"); 
