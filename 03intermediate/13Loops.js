b1e8a3df5ba99d733e1dd6b05d16b18fab9348c5
technophile22 <raghavmangal22@gmail.com>
2021-03-07 15:38:43 +0530

    loops

---
+++ 03intermediate/13Loops.js
@@ -0,0 +1,35 @@
+// for(let index = 0; index < 10; index++){
+//     console.log(index);
+// }
+
+
+const states = [
+    "Rajasthan",
+    "Delhi",
+    "Assam",
+    1947,
+    "Tamil Nadu",
+    "Maharashtra"
+];
+
+// for(let i = 0; i<states.length; i++)
+// {
+//     if(typeof states[i] !== 'string') continue;
+//     console.log(states[i]);
+// }
+
+// let i = 0;
+
+// while( i < states.length){
+//     console.log(states[i]);
+//     i++;
+// }
+
+
+let i = 20;
+
+do {
+    console.log(i);
+    i++;
+} while( i < 10);
+
---
+++ 03intermediate/14loops2.js
@@ -0,0 +1,12 @@
+
+const states = [
+    "Rajasthan",
+    "Delhi",
+    "Assam",
+    1947,
+    "Tamil Nadu",
+    "Maharashtra"
+];
+
+states.forEach( (s) => { console.log(s); } );
+
