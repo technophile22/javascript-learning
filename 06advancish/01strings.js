505d19309280e7312b01376dc50a091eea3a9771
technophile22 <raghavmangal22@gmail.com>
2021-03-10 15:35:51 +0530

    strings

---
+++ 06advancish/01strings.js
@@ -0,0 +1,19 @@
+var name = `raghav`;
+
+var fullName = `${name} mangal`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("l"));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("raghav"));
+console.log(fullName.includes("rgv"));
+console.log(fullName.toUpperCase());
+
+
+
+
+
+
+
+
